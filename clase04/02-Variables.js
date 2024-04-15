2ff72d9179c67ac9ca8c2ae22760f39625c12d48
Alejandro Di Battista <alejandrodibattista@gmail.com>
2024-04-15 01:10:39 -0300

    Clase 04

---
+++ clase04/02-Variables.js
@@ -0,0 +1,19 @@
+/// Variables
+// Son referencias a una memoria en donde se almacena un valor
+
+var viejo // Forma vieja de declarar variables, no usar
+
+let nombre = "Juan" // Forma moderna de declarar variables
+const NOMBRE = "Juan" // Forma moderna de declarar constantes
+
+// Son locales al archivo o al bloque en donde se declaran
+
+let a       // Declaración de variable
+typeof a    // "undefined"
+
+a = 1       // Asignación de valor
+
+let b = 2   // Declaración y asignación de variable
+
+let c = 3, d = 4 // Declaración y asignación de multiples variables
+
