a3597134ea6db9d18923287102cc8e9b98ab7485
Yapur <aleyapura2016@gmail.com>
2024-05-01 00:53:30 -0300

    Trabajo practico 1, agenda

---
+++ practicos/36 - 59314 - Yapura, Ramon Alejandro/TP1/index.jsx
@@ -0,0 +1,63 @@
+const Contactos = ({nombre, apellido, telefono}) => (
+    <div className="contacto">
+        <h3>{nombre} {apellido}</h3>
+        <p className="telefono">Teléfono: {telefono}</p>
+    </div>
+)
+
+const App = ({nombre}) =>  (
+    <div>
+        <h1>Agenda</h1>
+       <div id="contactos">
+       <Contactos
+        nombre = "Maria"
+        apellido = "Fernandez"
+        telefono="3817382758"
+        />
+        <Contactos
+        nombre = "Juan"
+        apellido = "Perez"
+        telefono="381485758"
+        />
+        <Contactos
+        nombre = "Pedro"
+        apellido = "Lopez"
+        telefono="381783945"
+        />
+        <Contactos
+        nombre = "Martina"
+        apellido = "Lopez"
+        telefono="381794545"
+        />
+        <Contactos
+        nombre = "Sofia"
+        apellido = "Dilascio"
+        telefono="381794545"
+        />
+        <Contactos
+        nombre = "Andres"
+        apellido = "Ruiz"
+        telefono="381794545"
+        />
+        <Contactos
+        nombre = "Rambo"
+        apellido = "Yepura"
+        telefono="381794545"
+        />
+        <Contactos
+        nombre = "Joaquin"
+        apellido = "Soria"
+        telefono="381794545"
+        />
+       </div>
+       <h2>Tarjeta Presentación</h2>
+       <div style={{display: "flex", justifyContent: "center"}}>
+            <div id="tarjeta">
+            <p><b>Ramón Alejandro Yapura</b></p>
+            <p>Teléfono:3815763886</p>
+            </div>
+        </div>
+    </div>
+)
+
+ReactDOM.render(<App />, document.getElementById('root'))
