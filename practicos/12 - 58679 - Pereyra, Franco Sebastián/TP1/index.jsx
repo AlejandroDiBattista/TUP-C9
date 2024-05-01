5ce90f070ca496b2e2d58bcae91702b5a7397443
FrancoPereyra2 <144034241+FrancoPereyra2@users.noreply.github.com>
2024-05-01 20:11:11 -0300

    TP1 Franco Pereyra

--- practicos/12 - 58679 - Pereyra, Franco Sebastián/TP1/index.jsx
+++ practicos/12 - 58679 - Pereyra, Franco Sebastián/TP1/index.jsx
@@ -1,8 +1,45 @@
-const App = () =>  (
-    <div>
-        <h1>¡Hola, mundo!</h1>
-        <p>¡Bienvenidos a React!</p>
+const contactos = [
+    {"id":1, "nombre":"Valentina", "apellido":"Hernandez", "telefono":"456321"},
+    {"id":0, "nombre":"Mateo", "apellido":"Gutierrez", "telefono":"987654"},
+    {"id":2, "nombre":"Luis", "apellido":"Santos", "telefono":"123789"},
+    {"id":3, "nombre":"Fernanda", "apellido":"Martinez", "telefono":"654321"},
+    {"id":4, "nombre":"Diego", "apellido":"Perez", "telefono":"789654"},
+    {"id":5, "nombre":"Julia", "apellido":"Diaz", "telefono":"654987"},
+    {"id":6, "nombre":"Gabriel", "apellido":"Torres", "telefono":"321456"},
+    {"id":7, "nombre":"Romina", "apellido":"Flores", "telefono":"789123"},
+    {"id":8, "nombre":"Nicolas", "apellido":"Alvarez", "telefono":"654123"},
+    {"id":9, "nombre":"Sol", "apellido":"Romero", "telefono":"321987"},
+    {"id":10, "nombre":"Lucas", "apellido":"Garcia", "telefono":"456789"},
+    {"id":11, "nombre":"Catalina", "apellido":"Rojas", "telefono":"987321"},
+    {"id":12, "nombre":"Martin", "apellido":"Lopez", "telefono":"456987"},
+    {"id":13, "nombre":"Belen", "apellido":"Mendez", "telefono":"789654"},
+    {"id":14, "nombre":"Maximiliano", "apellido":"Silva", "telefono":"654789"},
+    {"id":15, "nombre":"Constanza", "apellido":"Nuñez", "telefono":"321654"},
+    {"id":16, "nombre":"Tomas", "apellido":"Castro", "telefono":"987456"},
+    {"id":17, "nombre":"Julieta", "apellido":"Ortega", "telefono":"654123"},
+    {"id":18, "nombre":"Felipe", "apellido":"Ruiz", "telefono":"123654"},
+    {"id":19, "nombre":"Abril", "apellido":"Luna", "telefono":"789321"}
+    
+]
+const Contacto = ({nombre, Apellido, telefono}) => (
+    <div className="contactos">
+        <h2>{nombre} {Apellido}</h2>
+        <p>Telefono: {telefono}</p>
     </div>
 )
 
-ReactDOM.render(<App />, document.getElementById('root'))
+const Agenda = () => (
+    <div>
+        <h1>Agenda de contactos</h1>
+        {contactos.map(contacto => (
+            <Contacto nombre={contacto.nombre} 
+            Apellido={contacto.Apellido} 
+            telefono={contacto.telefono} />
+        ))}
+    </div>
+)
+
+
+ReactDOM.render(
+<Agenda />, 
+document.getElementById('root'))
