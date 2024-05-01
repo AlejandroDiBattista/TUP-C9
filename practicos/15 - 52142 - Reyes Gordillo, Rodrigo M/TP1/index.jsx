b2bec467545cbb9ba3576a944543e2333ebc889b
MaxR43 <maxireyesgordillo@gmail.com>
2024-05-01 14:32:52 -0300

    1er Commit
    Se carga primera versión de commit

--- practicos/15 - 52142 - Reyes Gordillo, Rodrigo M/TP1/index.jsx
+++ practicos/15 - 52142 - Reyes Gordillo, Rodrigo M/TP1/index.jsx
@@ -1,8 +1,29 @@
-const App = () =>  (
-    <div>
-        <h1>¡Hola, mundo!</h1>
-        <p>¡Bienvenidos a React!</p>
+function ContactCard(props) {
+    return (
+    <div className="contact-card">
+        <h2>{props.nombre} {props.apellido}</h2>
+        <p><strong>Teléfono:</strong> {props.telefono}</p>
     </div>
-)
+    );
+}
 
-ReactDOM.render(<App />, document.getElementById('root'))
+const contacts = [
+    { id: 1, nombre: 'Maxi', apellido: 'Reyes', telefono: '123-456-7890' },
+    { id: 2, nombre: 'Vale', apellido: 'Arrieta', telefono: '987-654-3210' },
+    { id: 2, nombre: 'Pedro', apellido: 'Pica Piedra', telefono: '3815-994-321' }
+];
+
+const App = () => {
+    return (
+    <div className="App">
+        <h1>Agenda de Contactos</h1>
+        <div className="contact-list">
+        {contacts.map(contact => (
+            <ContactCard key={contact.id} {...contact} />
+        ))}
+        </div>
+    </div>
+    );
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
