0f7d9a4043bf1381a8024d90cdf87fdfa20b5ed3
Yapur <aleyapura2016@gmail.com>
2024-05-01 14:41:19 -0300

    correccion del practico

--- practicos/36 - 59314 - Yapura, Ramon Alejandro/TP1/index.jsx
+++ practicos/36 - 59314 - Yapura, Ramon Alejandro/TP1/index.jsx
@@ -1,63 +1,37 @@
-const Contactos = ({Id, nombre, apellido, telefono}) => (
+const contactos = [
+    {nombre: "Alejandro", apellido: "Yapura", telefono: "3815763886"},
+    {nombre: "Maria" , apellido: "Fernandez", telefono: "3815943758"},
+    {nombre: "Juan", apellido:"Perez", telefono:"381485758"},
+    {nombre: "Pedro", apellido: "Lopez", telefono:"381783945"},
+    {nombre: "Joaquin", apellido: "Soria", telefono:"381384758"},
+    {nombre: "Jose", apellido: "Gonzalez", telefono:"381481458"},
+    {nombre: "Ana", apellido: "Martinez", telefono: "3817654321"},
+    {nombre: "Luis", apellido: "Rodriguez", telefono: "3819876543"},
+    {nombre: "Sofia", apellido: "Dilascio", telefono: "3815643219"},
+    {nombre: "Diego", apellido: "Torres", telefono: "3813219236"},
+    {nombre: "Laura", apellido: "Ramirez", telefono: "3817132234"}
+]
+
+const Contacto = ({id, nombre, apellido, telefono}) => (
     <div className="contacto">
         <h3>{nombre} {apellido}</h3>
         <p className="telefono">Teléfono: {telefono}</p>
-        <p className="id">Id: {Id}</p>
+        <p className="id">id:{id}</p>
     </div>
 )
 
-const App = ({nombre}) =>  (
+const App = () =>  (
     <div>
         <h1>Agenda</h1>
        <div id="contactos">
-       <Contactos
-        Id = "0"
-        nombre = "Maria"
-        apellido = "Fernandez"
-        telefono="3817382758"
-        />
-        <Contactos
-        Id = "1"
-        nombre = "Juan"
-        apellido = "Perez"
-        telefono="381485758"
-        />
-        <Contactos
-        Id = "2"
-        nombre = "Pedro"
-        apellido = "Lopez"
-        telefono="381783945"
-        />
-        <Contactos
-        Id = "3"
-        nombre = "Martina"
-        apellido = "Lopez"
-        telefono="381794545"
-        />
-        <Contactos
-        Id = "4"
-        nombre = "Sofia"
-        apellido = "Dilascio"
-        telefono="381794545"
-        />
-        <Contactos
-        Id = "5"
-        nombre = "Andres"
-        apellido = "Ruiz"
-        telefono="381794545"
-        />
-        <Contactos
-        Id = "6"
-        nombre = "Rambo"
-        apellido = "Yepura"
-        telefono="381794545"
-        />
-        <Contactos
-        Id = "7"
-        nombre = "Joaquin"
-        apellido = "Soria"
-        telefono="381794545"
-        />
+        {contactos.map((contacto, index) => (
+            <Contacto
+                nombre={contacto.nombre}
+                apellido={contacto.apellido}
+                telefono={contacto.telefono}
+                id={index}
+            />
+        ))}
        </div>
        <h2>Tarjeta Presentación</h2>
        <div style={{display: "flex", justifyContent: "center"}}>
