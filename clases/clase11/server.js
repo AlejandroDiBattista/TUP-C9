34835e70c2f97eab6407429df0d0b20faa4e7c9f
Alejandro Di Battista <alejandrodibattista@gmail.com>
2024-06-02 00:29:13 -0300

    .

--- clases/clase11/server.js
+++ clases/clase11/server.js
@@ -3,13 +3,11 @@ import express from "express"
 import router from "./rutas.js"
 import noCincos from "./midlerware.js"
 import morgan from "morgan"
-import cors from "cors"
 
 const app = express()
 
 app.use(express.json())
 app.use(morgan('dev'))
-app.use(cors())
 
 app.use((req, res, next) => {
     console.log("Request received", req.method, req.url, req.body)
@@ -20,6 +18,32 @@ app.use((req, res, next) => {
     res.statusMessage, 
     res.getHeaders())
 })
+
+let productos = [
+    {id: 1, precio: 200, nombre: "Coca Cola", cantidad: 10},
+    {id: 2, precio: 150, nombre: "Pepsi", cantidad: 10},
+    {id: 3, precio: 100, nombre: "Fanta", cantidad: 10},
+]
+// REST 
+// CRUD
+// CREATE
+app.get("/productos", (req, res) => {
+    res.json(productos)
+}   )
+
+app.post("/productos", (req, res) => {
+    let producto = req.body
+productos = [...productos, producto];    
+res.json(producto)
+})
+
+app.delete("/productos/:id", (req, res) => {
+    let id = req.params.id
+    productos = productos.filter(producto => producto.id != id)
+    res.json({message: "Producto eliminado"})
+}
+)
+
 
 app.use(noCincos)
 
