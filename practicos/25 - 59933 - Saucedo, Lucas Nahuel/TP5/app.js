6b35d126cf709ce0a2e5f4634b796939637f2840
Yayo <lucasctmn@gmail.com>
2024-06-08 08:53:09 -0300

    tp5-ls
    saucedo lucas

--- practicos/25 - 59933 - Saucedo, Lucas Nahuel/TP5/app.js
+++ practicos/25 - 59933 - Saucedo, Lucas Nahuel/TP5/app.js
@@ -7,15 +7,50 @@ app.use(express.json());
 app.use(cors())
 
 let datos = [
-    // Datos de ejemplo   
+    { id: 0, nombre: 'Marta', apellido: 'Wirth', edad: 25, borrado: false, actualizado: Date.now() },
+    { id: 1, nombre: 'Lucas', apellido: 'Rodriguez', edad: 28, borrado: false, actualizado: Date.now() },
+    { id: 2, nombre: 'Carlos', apellido: 'Saucedo', edad: 24, borrado: false, actualizado: Date.now() },
+    { id: 3, nombre: 'Juan', apellido: 'Perez', edad: 35, borrado: false, actualizado: Date.now() },
+    { id: 4, nombre: 'Ana', apellido: 'Bolea', edad: 31, borrado: false, actualizado: Date.now() },
 ]
 
 app.get('/personas', (req, res) => {
-    // Implementar GET_ALL
-});
+        const personaAct = datos.filter(persona => !persona.borrado);
+        res.status(200).json(personaAct);
+    });
 
 app.put('/personas', (req, res) => {
-    // Implementar PUT
-})
+    const { id, nombre, apellido, edad, borrado } = req.body;
+
+    if (id) {
+        const personaIndex = datos.findIndex(persona => persona.id === id);
+
+        if (personaIndex !== -1) {
+            if (borrado) {
+                datos[personaIndex].borrado = true;
+            } else {
+                if (nombre) datos[personaIndex].nombre = nombre;
+                if (apellido) datos[personaIndex].apellido = apellido;
+                if (edad !== undefined) datos[personaIndex].edad = edad;
+            }
+            datos[personaIndex].actualizado = Date.now();
+            return res.status(201).json(datos[personaIndex]);
+        } else {
+            return res.status(404).send('Persona no encontrada');
+        }
+    } else {
+        const nuevaId = datos.length ? Math.max(...datos.map(persona => persona.id)) + 1 : 1;
+        const nuevaPersona = {
+            id: nuevaId,
+            nombre,
+            apellido,
+            edad,
+            borrado: false,
+            actualizado: Date.now()
+        };
+        datos.push(nuevaPersona);
+        return res.status(201).json(nuevaPersona);
+    }
+});
 
 export default app
