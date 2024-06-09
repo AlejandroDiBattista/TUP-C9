045a1dca4c91c920149a10bda2e10626199bb542
micsbrocco <micaelasbrocco02@gmail.com>
2024-06-09 18:22:02 -0300

    resoluciontp5

--- practicos/26 - 59320 - Sbrocco, Micaela/TP5/app.js
+++ practicos/26 - 59320 - Sbrocco, Micaela/TP5/app.js
@@ -7,15 +7,45 @@ app.use(express.json());
 app.use(cors())
 
 let datos = [
-    // Datos de ejemplo   
+    { id: 1, nombre: "Juan", apellido: "Pérez", edad: 30 },
+    { id: 2, nombre: "Ana", apellido: "Gómez", edad: 25 },
+    { id: 3, nombre: "Carlos", apellido: "López", edad: 35 },
+    { id: 4, nombre: "María", apellido: "Rodríguez", edad: 28 },
+    { id: 5, nombre: "Luis", apellido: "Martínez", edad: 40 },
+    { id: 6, nombre: "Laura", apellido: "Fernández", edad: 22 } 
 ]
 
 app.get('/personas', (req, res) => {
-    // Implementar GET_ALL
+    const pNoBorradas = datos.filter(p=> !p.borrado)
+    res.json(pNoBorradas)
 });
 
 app.put('/personas', (req, res) => {
-    // Implementar PUT
+    const persona = req.body;
+    const id = persona.id;   
+
+    if (!id){
+        const newId = Math.max(...datos.map(p=> p.id)) + 1
+        persona.id = newId
+        datos.push(persona)
+        res.json(persona)
+    }
+    else {
+        const index = datos.findIndex (p => p.id == id)
+        if (index >= 0) {
+            if(persona.borrado === true ){
+                datos[index].borrado = true;
+            }
+            else {
+                datos[index] = {...datos[index], ...persona}
+            }
+            res.json(datos[index])
+        }
+        else {
+            datos.push(persona)
+            res.json(persona)
+        }
+    }
 })
 
 export default app
