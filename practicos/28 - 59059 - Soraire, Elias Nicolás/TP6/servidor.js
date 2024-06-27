d198c353b9ec8201c221cfc19dad72c9ffac97e7
Lucas Gastón <lucasgastonvillafane@gmail.com>
2024-06-26 23:29:27 -0300

    EliasTP6
    Buenas noches profesor, se que ya paso la fecha de entrega pero eh tenido un par de problemas, el error es mío al no avisarle, espero que me pueda aceptar este proyecto, desde ya muchas gracias

--- practicos/28 - 59059 - Soraire, Elias Nicolás/TP6/servidor.js
+++ practicos/28 - 59059 - Soraire, Elias Nicolás/TP6/servidor.js
@@ -1,15 +1,56 @@
-import express from 'express';
-import cookieParser from 'cookie-parser';
-import morgan
- from 'morgan';
+const express = require("express");
 const app = express();
+const session = require("express-session");
+const bodyParser = require("body-parser");
+const path = require("path");
+const puerto = 3001;
 
-app.use(morgan('dev'));     // Loggea cada request en consola
-app.use(cookieParser());    // Para leer cookies
-app.use(express.json());    // Para leer JSONs
-app.use(express.static('public'));  // Para servir archivos estáticos
+app.use(bodyParser.json());
+app.use(session({
+    secret: 'mi_secreto',
+    resave: false,
+    saveUninitialized: true
+}));
 
-// Implementar las rutas necesarias
-app.listen(3000, () => {
-    console.log('Servidor iniciado en http://localhost:3000');
+let usuarios = [];
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.post('/api/registro', (req, res) => { 
+    const { nombreUsuario, contrasena } = req.body;
+    if (usuarios.find(usuario => usuario.nombreUsuario === nombreUsuario)) {
+        return res.status(400).json({mensaje: 'El usuario ya existe'});
+    }
+    usuarios.push({nombreUsuario, contrasena});
+    return res.status(200).json({mensaje: 'Usuario registrado exitosamente'});
+});
+
+app.post('/api/inicio-sesion', (req, res) => {  
+    const { nombreUsuario, contrasena } = req.body;
+    const usuario = usuarios.find(usuario => usuario.nombreUsuario === nombreUsuario && usuario.contrasena === contrasena);
+    if (!usuario) {
+        return res.status(401).json({mensaje: 'Credenciales incorrectas'});
+    }
+    req.session.usuario = usuario;
+    res.json({mensaje: 'Inicio de sesión exitoso', usuario});
+});
+
+app.post('/api/cierre-sesion', (req, res) => {  
+    req.session.destroy();
+    res.json({mensaje: 'Cierre de sesión exitoso'});
+});
+
+app.get('/api/info', (req, res) => {
+    if (!req.session.usuario) {
+        return res.status(401).json({mensaje: 'No autorizado'});
+    }
+    res.json({ usuario: req.session.usuario });
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.listen(puerto, () => {
+    console.log(`Servidor escuchando en http://localhost:${puerto}`);
 });
